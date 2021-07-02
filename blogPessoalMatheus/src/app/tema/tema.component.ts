f72b3ff37c30dbee9a564ebab6e0cc1258bb3d59
Matheus094-tech <80859363+Matheus094-tech@users.noreply.github.com>
2021-07-02 09:20:15 -0300

    Post e get de tema funcionaaaaais

--- blogPessoalMatheus/src/app/tema/tema.component.ts
+++ blogPessoalMatheus/src/app/tema/tema.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -8,17 +11,42 @@ import { Router } from '@angular/router';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema ()
+  listaTemas: Tema[]
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
-  ngOnInit(){
+  ngOnInit(){ //executa sempre que iniciar a página
 
-    /* if (environment.token == ''){
+    if (environment.token == ''){
       alert('Sua sessão expirou, logue novamente.')
       this.router.navigate(['/entrar'])
-    } */
-    
+    }
+
+    this.findAllTemas()
   }
+
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema [])=>{
+      this.listaTemas = resp
+    })
+  }
+
+
+
+
+  postarTema(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
+  }
+
 
 }
