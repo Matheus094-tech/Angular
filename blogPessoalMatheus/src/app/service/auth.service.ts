8bd1c1aa830b9aa6504badb876d36e514455354f
Matheus094-tech <matheusbarbosa094@gmail.com>
2022-01-03 09:13:48 -0300

    atualizar

--- blogPessoalMatheus/src/app/service/auth.service.ts
+++ blogPessoalMatheus/src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,13 +14,22 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+
   entrar (userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuario/logar', userLogin)
+    return this.http.post<UserLogin>('http://localhost:8080/usuario/login', userLogin)
   }
 
   cadastrar (user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuario', user)
     
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`http://localhost:8080/usuario/${id}`, this.token)
   }
 
   logado(){
